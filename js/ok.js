eea01eef70180f0d356dbf28191b7462c9afa29c
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2022-09-01 11:15:10 +0200

    Update ok.js

--- js/ok.js
+++ js/ok.js
@@ -1,23 +1,23 @@
-// function createGames(gamesList) {
-//   let html = "";
+function createGames(arry) {
+  let html = "";
 
-//   for (let i = 0; i < gamesList.length; i++) {
-//     let releaseYear = "Unknown year";
+  for (let i = 0; i < arry.length; i++) {
+    let hasRelease = "Unknown year";
 
-//     if (gamesList[i].released) {
-//       releaseYear = gamesList[i].released;
-//     }
+    if (arry[i].released) {
+      hasRelease = arry[i].released;
+    }
 
-//     html += `<div class="game">
-//                     <h5>${gamesList[i].name}</h5>
-//                     <p>Released: ${releaseYear}</p>
-//                 </div>`;
-//   }
+    html += `<div class="game">
+                    <h5>${arry[i].name}</h5>
+                    <p>Released: ${hasRelease}</p>
+                </div>`;
+  }
 
-//   return html;
-// }
+  return html;
+}
 
-// const newHTML = createGames(games);
+const newHTML = createGames(games);
 
-// const gameContainer = document.querySelector(".game-container");
-// gameContainer.innerHTML = newHTML;
+const gameContainer = document.querySelector(".game-container");
+gameContainer.innerHTML = newHTML;
